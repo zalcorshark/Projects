271913bf8c2ba142379292d6d34a4e033cb4780f
zalcorshark <tjk67@zips.uakron.edu>
2014-12-10 22:59:13 -0800

    Updating with files

---
+++ Workout/main.cpp
@@ -0,0 +1,18 @@
+/*
+This is a personal project to take how much I have worked out this past week and compare it to
+how much I worked out the week before
+
+Author: Thomas Kennedy
+Date: 11/13/2014
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ finalexamtestproject/finalexamtestproject/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float num =0;
+
+    float *fltptr = NULL;
+
+    *fltptr = num;
+
+    return 0;
+}
